8e263bdfa8fb64330d1dca87672e35876e80b816
John Chilton <jmchilton@gmail.com>
2011-12-11 21:44:48 -0600

    Small fix to MySQL syntax of tag migration.

--- projects/TropixUofMModels/src/orm/edu/umn/msi/tropix/models/migrations/V1_17_5__Add_Tags.sql
+++ projects/TropixUofMModels/src/orm/edu/umn/msi/tropix/models/migrations/V1_17_5__Add_Tags.sql
@@ -1,4 +1,4 @@
-create table TAG (ID varchar(255) not null, TAG_NAME varchar(255));
+create table TAG (ID varchar(255) not null, TAG_NAME varchar(255), primary key (ID));
 
 create table JOIN_TAG_OBJECT (TAG_ID varchar(255) not null, OBJECT_ID varchar(255) not null, primary key (TAG_ID, OBJECT_ID)); 
 ${if_mysql} ALTER TABLE JOIN_TAG_OBJECT ENGINE = INNODB;
